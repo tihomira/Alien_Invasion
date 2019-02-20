7cff12180594d16bf2e1299e2abcb87c74372fa1
Tihomira Nikolova <tiholova8@gmail.com>
2019-02-20 14:17:17 +0100

    a small commit before pushing to remote.

--- Alien_Invasion/alien_invasion.py
+++ Alien_Invasion/alien_invasion.py
@@ -1,14 +1,16 @@
 import sys
+
 import pygame
+
 from settings import Settings
+
 from ship import Ship
 
 def run_game():
 	# Initialize pygame, settings, and screen object.
 	pygame.init()
 	ai_settings = Settings()
-	screen = pygame.display.set_mode(
-		(ai_settings.screen_width, ai_settings.screen_height))
+	screen = pygame.display.set_mode((ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
 
 	# Make a ship
@@ -29,4 +31,5 @@ def run_game():
 
 		# Make the most recently drawn screen visible.
 		pygame.display.flip()
+
 run_game()
