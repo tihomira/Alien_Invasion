b6eac281e53a891243438e92bb35201ff8c2bba1
Tihomira Nikolova <overthegame.tihomira@gmail.com>
2019-02-20 13:14:32 +0100

    Ship class integrated.

--- Alien_Invasion/alien_invasion.py
+++ Alien_Invasion/alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 import pygame
 from settings import Settings
+from ship import Ship
 
 def run_game():
 	# Initialize pygame, settings, and screen object.
@@ -9,6 +10,9 @@ def run_game():
 	screen = pygame.display.set_mode(
 		(ai_settings.screen_width, ai_settings.screen_height))
 	pygame.display.set_caption("Alien Invasion")
+
+	# Make a ship
+	ship = Ship(screen)
 
 
 	# Start the main loop for the game.
@@ -21,6 +25,7 @@ def run_game():
 
 		# Redraw the screen during each pass through the loop.
 		screen.fill(ai_settings.bg_color)
+		ship.blitme()
 
 		# Make the most recently drawn screen visible.
 		pygame.display.flip()
